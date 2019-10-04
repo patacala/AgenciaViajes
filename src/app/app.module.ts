d305ce696d7c45983769ef28c5fedda158afc062
peterbarranco <peterb.alfaro@gmail.com>
2019-10-03 22:54:41 -0500

    Modulos actualizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// modulos
+import { PagesModule } from './pages/pages.module';
+
+// rutas
+import { APP_ROUTES } from './app.routes';
+
+// componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NotpagefoundComponent } from './shared/notpagefound/notpagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NotpagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
