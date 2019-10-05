2e4796d98b637b06230b33a4376f34b4c66f70b9
peterbarranco <peterb.alfaro@gmail.com>
2019-10-05 00:00:35 -0500

    update breadcrumbs and metatags

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -11,9 +11,9 @@ const pagesRoutes: Routes = [
         path: '',
         component: PagesComponent,
         children: [
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'graficas1', component: Graficas1Component},
+            {path: 'dashboard', component: DashboardComponent, data: {titulo: 'Dashboard'}},
+            {path: 'progress', component: ProgressComponent, data: {titulo: 'Progress'}},
+            {path: 'graficas1', component: Graficas1Component, data: {titulo: 'Graficas'}},
             {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
         ]
     },
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  titulo: string;
+  constructor(private router: Router,
+              private title: Title,
+              private meta: Meta) {
+
+    this.getDataRoute().subscribe(data => {
+      // setea el titulo del breadcrumbs
+      this.titulo = data.titulo;
+      // se setea el titulo de la pestaña del navegador
+      this.title.setTitle(this.titulo);
+
+      const metatag: MetaDefinition = {
+        name: 'descripcion',
+        content: this.titulo
+      };
+
+      this.meta.updateTag(metatag);
+
+
+
+
+    });
+   }
 
   ngOnInit() {
+  }
+
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter( evt => evt instanceof ActivationEnd),
+      filter( (evt: ActivationEnd) => evt.snapshot.firstChild === null),
+      map((evt: ActivationEnd ) => evt.snapshot.data)
+    );
   }
 
 }
